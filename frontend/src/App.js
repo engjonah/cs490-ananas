f0a83dedd632c82f4941b5b867261c6a5ac1a807
Jonah Eng <engjonah@gmail.com>
2024-02-16 10:56:28 -0500

    pull api from react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const API_BASE_URL = process.env.NODE_ENV === 'production' ?
+    'https://cs490-ananas-2b50a43be02e.herokuapp.com' :
+    'http://localhost:3000';
+
+  let [test, setTest] = useState(null);
+
+  useEffect(()=>{
+    fetch(`${API_BASE_URL}/test`)
+      .then(res=>res.json())
+      .then(res=>{
+        setTest(res)
+      })
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +23,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {!test && <p>API not working</p>}
+        {test && test.map((item, index) => (<p>{item.test}</p>))}
         <a
           className="App-link"
           href="https://reactjs.org"
