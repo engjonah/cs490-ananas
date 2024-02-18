cae7f85b853003d34b013cc79274c8010e770e82
Jonah Eng <engjonah@gmail.com>
2024-02-17 23:34:33 -0500

    [CA-25] Sample unit test (#7)
    [Problem]
    BackendStatus component needs unit tests, also project needs unit testing example.
    [Solution]
    Add 3 unit tests for BackendStatus component.

---
+++ frontend/src/__tests__/BackendStatus.test.js
@@ -0,0 +1,23 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import BackendStatus from "../components/BackendStatus";
+
+afterEach(() => {
+  cleanup(); 
+})
+
+describe("BackendStatus Component", () => {
+  test('backend status text rending', () => {
+    render(<BackendStatus status={[{_id: 'asdf123', test: 'hello wolrd'}]}/>);
+    expect(screen.getByText('Backend Status:')).toBeInTheDocument();
+  })  
+  test('backend status false', () => {
+    render(<BackendStatus status={null}/>);
+    expect(screen.getByText('API not working')).toBeInTheDocument();
+  }) 
+  test('backend status true', () => {
+    render(<BackendStatus status={[{_id: 'asdf123', test: 'hello wolrd'}]}/>);
+    expect(screen.getByText('hello wolrd')).toBeInTheDocument();
+  }) 
+})
+
+
--- frontend/src/components/BackendStatus.js
+++ frontend/src/components/BackendStatus.js
@@ -3,7 +3,7 @@ export default function BackendStatus({status}) {
     <>
       <p>Backend Status:</p>
       {!status && <p>API not working</p>}
-      {status && status.map((item, index) => (<p id={index}>{item.test}</p>))}
+      {status && status.map((item, index) => (<p key={index}>{item.test}</p>))}
     </>
   )
 }
