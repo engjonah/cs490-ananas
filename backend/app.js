0df68f41aed55193115e66405cb2465e7d9a595b
Jonah Eng <engjonah@gmail.com>
2024-02-18 22:28:28 -0500

    move heroku code block back into server.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors');
 const connectDB = require("./db/conn");
 const router = require('./routes');
-require('dotenv').config();
 
 const app = express()
 
@@ -10,19 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
-
-const path = require("path");
-
-//link frontend on heroku
-if (process.env.NODE_ENV === "production") {
-
-    app.use(express.static("frontend/build"));
-
-    app.get("*", (req, res) => {
-
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-   });
-}
 
 app.use('/', router);
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,19 @@ const app = require('./app');
 
 require('dotenv').config();
 
+const path = require("path");
+
+//link frontend on heroku
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+   });
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
