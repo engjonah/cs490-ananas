aab5089407d9d9c0ef87e18dda72fd8a474a606e
Jonah Eng <engjonah@gmail.com>
2024-02-18 22:31:41 -0500

    add express import

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-const express = require('express')
 const cors = require('cors');
 const connectDB = require("./db/conn");
 const router = require('./routes');
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-
+const express = require('express')
 require('dotenv').config();
 
 const path = require("path");
@@ -7,16 +7,15 @@ const path = require("path");
 //link frontend on heroku
 if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static("frontend/build"));
+  app.use(express.static("frontend/build"));
 
-    app.get("*", (req, res) => {
-
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-   });
+  });
 }
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
