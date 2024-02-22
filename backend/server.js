2f02b1996a49183228d695721a50427c70b417e7
DantheOPMan <soccerdan34@gmail.com>
2024-02-22 14:15:22 -0500

    undid server.js change
    im retarded

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,20 @@
 const app = require('./app');
-const express = require('express');
-const path = require('path');
+const express = require('express')
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const path = require("path");
 
-// Serve static files from the 'frontend/build' directory in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+//link frontend on heroku
+if (process.env.NODE_ENV === "production") {
 
-  // Define routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
-// Define all other routes before this runs
-app.use('*', (req, res) => {
-  res.status(404).send('Route not found');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Internal Server Error');
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
