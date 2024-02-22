46883055b011551750a4a4a235fecb1966714878
DantheOPMan <soccerdan34@gmail.com>
2024-02-22 13:52:49 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
 const app = require('./app');
-const express = require('express')
+const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
-const path = require("path");
+const port = process.env.PORT || 3000;
 
-//link frontend on heroku
-if (process.env.NODE_ENV === "production") {
+// Serve static files from the 'frontend/build' directory in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-  app.use(express.static("frontend/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  // Define routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
-const port = process.env.PORT || 3000;
+// Define all other routes before this runs
+app.use('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
