07a00192ab45e3263a0802fad161d28812b270cb
DantheOPMan <soccerdan34@gmail.com>
2024-02-22 13:01:30 -0500

    added navbar
    added navbar and routing functionality

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -3,16 +3,15 @@ import React from 'react';
 const HomeScreen = () => {
   return (
     <div>
-      <h1>How to Use</h1>
+      <h2>How to Use</h2>
       <ol>
-        <li>Step 1: Lorem ipsum dolor sit amet.</li>
-        <li>Step 2: Consectetur adipiscing elit.</li>
-        <li>Step 3: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-        <li>Step 4: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+        <li>Select Source Language</li>
+        <li>Select Destination Item</li>
+        <li>Select Convert</li>
+        <li>That's It!</li>
       </ol>
       <h2>Overview</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      {/* Add more content or components as needed */}
+      <p>Tool to quickly and accurately convert code from one language to another utilizing ChatGPT 3.5</p>
     </div>
   );
 }
