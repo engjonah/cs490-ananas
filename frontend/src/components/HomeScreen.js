909e6f75aae236e98e31c4d58a65a5e8300e2970
DantheOPMan <soccerdan34@gmail.com>
2024-02-22 12:26:44 -0500

    homescreen added
    hoemscreen

---
+++ frontend/src/components/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>How to Use</h1>
+      <ol>
+        <li>Step 1: Lorem ipsum dolor sit amet.</li>
+        <li>Step 2: Consectetur adipiscing elit.</li>
+        <li>Step 3: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+        <li>Step 4: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+      </ol>
+      <h2>Overview</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      {/* Add more content or components as needed */}
+    </div>
+  );
+}
+
+export default HomeScreen;
