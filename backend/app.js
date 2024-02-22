cf47e30f43aeab68705ad5f11dfe604512f7f121
DantheOPMan <soccerdan34@gmail.com>
2024-02-22 14:23:40 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
 const cors = require('cors');
 const connectDB = require("./db/conn");
 const router = require('./routes');
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 
+// Database connection
 connectDB();
 
-app.use('/', router);
+// API routes
+app.use('/api', router);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Ensure that all other routes are handled by your React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 
 module.exports = app;
