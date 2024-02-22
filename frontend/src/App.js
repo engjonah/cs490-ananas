36ef1389204fa870d172461c17069983de7fc072
DantheOPMan <44145044+DantheOPMan@users.noreply.github.com>
2024-02-22 16:20:37 -0500

    [CA-41] Added navbar and homescreen (#12)
    [Problem]
    Wanted to add the Home Landing Page, but didn't want to just add it on top of App.js
    
    [Solution]
    -Moved current `App.js` to `TranslatePage.js` within the components folder
    -Added routing to `App.js` and added a navbar. Had to `npm install react-router-dom` for routing to work.
    -Made a `Navbar.js`  within the components folder and created a very basic navbar that routes to the home screen and MainApp page.
    -Moved `App.css` into components. Would recommend renaming it to `TranslatePage.css` to fit with the `TranslatePage.js` component.
    -Created very basic `HomeScreen.js` page as the landing page. Has no css/design yet. Will go in and change that later during this sprint.
    
    Co-authored-by: Jonah Eng <engjonah@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Unstable_Grid2';
-import BackendStatus from './components/BackendStatus';
-import CodeSubmissionBox from './components/CodeSubmissionBox';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TranslatePage from './components/TranslatePage';
+import HomeScreen from './components/HomeScreen';
+import Navbar from './components/Navbar';
 
 function App() {
-  const API_BASE_URL = process.env.NODE_ENV === 'production' ?
-    window.location.origin:
-    'http://localhost:3000';
-
-  let [test, setTest] = useState(null);
-
-  useEffect(()=>{
-    fetch(`${API_BASE_URL}/api/test`)
-      .then(res=>res.json())
-      .then(res=>{
-        setTest(res)
-      })
-  },[API_BASE_URL]);
-  
   return (
-    <div className="App">
-      <div className="App-header">
-        <div id='navbar'>Navbar Placeholder</div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/translate" element={<TranslatePage />} />
+        </Routes>
       </div>
-      <div className="div">
-        <Container id="translation" maxWidth="lg" style={{"backgroundColor":"gray"}}>
-          <Grid container spacing={2}>
-            <Grid xs={6}>
-              <Container maxWidth="sm" style={{"backgroundColor":"blue", "display": "inline-block", "minHeight": "10vh"}}>
-                Code upload placeholder
-              </Container>
-              <Container maxWidth="sm" style={{"backgroundColor":"green", "display": "inline-block", "minHeight": "50vh"}}>
-                Code Input placeholder  
-                <CodeSubmissionBox />
-              </Container>
-            </Grid>
-            <Grid xs={6}>
-              <Container maxWidth="sm" style={{"backgroundColor":"red", "display": "inline-block", "minHeight": "60vh"}}>
-                Code Output placeholder
-              </Container>
-            </Grid>
-          </Grid>
-        </Container>
-        <BackendStatus status={test}/>
-      </div>
-    </div>
+    </Router>
   );
 }
 
