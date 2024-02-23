8f7f8ad270c9490bb1e1fa490cab423233474970
Saumya Dwivedi <saumyad03@gmail.com>
2024-02-23 17:09:46 -0500

    [CA-42] Implement navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import { Icon } from './Icon.js'
+import { ReactComponent as TranslateIcon } from '../icons/TranslateIcon.svg'
+import { ReactComponent as DocumentationIcon } from '../icons/DocumentationIcon.svg'
+import { ReactComponent as AccountIcon } from '../icons/AccountIcon.svg'
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/translate">Translate</Link>
-        </li>
-      </ul>
-    </nav>
+    <AppBar position="static" sx={{ bgcolor: '#D9D9D9' }}>
+      <Toolbar>
+        <Link style={{ color: 'inherit', textDecoration: 'none' }} to="/">
+          <Typography variant='h4' style={{ color: 'black', fontFamily: 'Fira Code' }} edge="start">Ananas</Typography>
+        </Link>
+        <div style={{ flexGrow: 1 }} />
+        <Link to="/translate">
+          <Icon icon={TranslateIcon} />
+        </Link>
+        <Link to="/documentation">
+          <Icon icon={DocumentationIcon} />
+        </Link>
+        <Link to="/account">
+          <Icon icon={AccountIcon} />
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 }
 
