eacedd23c9863d25538a8cde7ec3515c8fa657cd
Harshit Panwar <panwar.h.one@gmail.com>
2024-02-23 14:56:37 -0500

    [CA-39][CA-40] Implement registration in firebase+mongo (email/pw) (#16)
    [Problem]
    Needed to connect web app to Firebase, use Firebase Auth for registration, store user entries in Mongo
    [Solution] 
    - Created a Firebase Project through Firebase console and added config file to `frontend/src/firebase.js` with logic to register user through Firebase Auth and send user info to MongoDB
    - Added `serviceAccount` in `backend/firebase.js` for future Google sign in implementation
    - Had to `npm install firebase` and `npm install react-router-dom` in frontend and `npm install firebase` in backend
    - Created a Sign-up component ( temporary form ) for registration UI
    - Inserted Sign-up component in `App.js`
    - Created controller and route for registration
    - Created temporary user model
    
    Co-authored-by: Jonah Eng <engjonah@gmail.com>

---
+++ frontend/src/firebase.js
@@ -0,0 +1,55 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRAAMyL5qQ5wK2gS4NHMjk1GVmmSVD2Is",
+  authDomain: "cs490-ananas.firebaseapp.com",
+  projectId: "cs490-ananas",
+  storageBucket: "cs490-ananas.appspot.com",
+  messagingSenderId: "355817163789",
+  appId: "1:355817163789:web:99a8f8da64ed92f0c61944",
+  measurementId: "G-HRBMXWQBN4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const registerUserToMongo = async(name,email,uid) =>{
+    const API_BASE_URL = process.env.NODE_ENV === 'production' ?
+     window.location.origin:
+     'http://localhost:3000';
+    await fetch(`${API_BASE_URL}/api/register`,{
+        method: "POST",
+        body: JSON.stringify({
+            name,
+            email,
+            uid,
+        }),
+        headers:{
+            "Content-type": "application/json"
+        },
+    })
+    .then(() => {
+        console.log("User registered");
+    })
+    .catch((err) => {
+        console.log(err.message)
+    })
+};
+
+const registerWithEmailAndPassword = async(name,email,password) => {
+    try{
+        const response = await createUserWithEmailAndPassword(auth, email, password)
+        const user = response.user
+        await registerUserToMongo(name, email, user.uid)
+    }catch (error){
+        console.log(error.message);
+    }
+}
+export { app , auth , registerWithEmailAndPassword };
