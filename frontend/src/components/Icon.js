91f0dcb2a1387732afef964f9a5e80e3258204e2
Jonah Eng <engjonah@gmail.com>
2024-02-23 23:58:17 -0500

    nit tabbing stuff

--- frontend/src/components/Icon.js
+++ frontend/src/components/Icon.js
@@ -1,11 +1,11 @@
 import { Tooltip, IconButton, SvgIcon } from '@mui/material'
 
 export function Icon(props) {
-    return (
-        <Tooltip title={props.tooltip}>
-        <IconButton>
-            <SvgIcon component={props.icon} />
-        </IconButton>
-        </Tooltip>
-    );
+  return (
+    <Tooltip title={props.tooltip}>
+      <IconButton>
+        <SvgIcon component={props.icon} />
+      </IconButton>
+    </Tooltip>
+  );
 }
