6f9b91269e3079c7375399d2fa3b1a19fa552bf8
Jaden Nguyen <71035730+21jadennguyen@users.noreply.github.com>
2024-02-26 10:55:05 -0500

    [CA-59] [CA-53] [CA-54] Added components for copy button, download button, and language tabs (#21)
    [Problem]
    The user needs options to copy and download the code. They also need to be able to choose the input and output programming language.
    [Solution] 
    - Add Language selection buttons in `CodeBox` component
    - Add IconButton's for Download and Copy functionalities in CodeBox component
    - Dynamically change file extension of downloaded file depending on selected language
    
    Co-authored-by: Jonah Eng <engjonah@gmail.com>

--- frontend/src/components/CodeBox.js
+++ frontend/src/components/CodeBox.js
@@ -1,15 +1,20 @@
 import Editor from '@monaco-editor/react';
 import React, { useRef, useState } from 'react';
 import { Button, Container, Tooltip }  from '@mui/material';
+import { IconButton, Box, Tab, Tabs } from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 
 export default function CodeSubmissionBox({defaultValue, readOnly}) {
   const editorRef = useRef(null);
 
   let [inputExists, setInputExists] = useState(false)
+  const [code, setCode] = useState(defaultValue);
+  const [currTab, setCurrTab] = React.useState(0);
 
   function updateCurrentInput() {
-    const currValue = editorRef.current.getValue();
-    setInputExists(readOnly || (currValue !== defaultValue && currValue !== ''));
+    setCode(editorRef.current.getValue());
+    setInputExists(readOnly || (code !== defaultValue && code !== ''));
   }
 
   function handleEditorDidMount(editor, monaco) {
@@ -18,12 +23,53 @@ export default function CodeSubmissionBox({defaultValue, readOnly}) {
   }
 
   function showValue() {
-    alert(editorRef.current.getValue());
+    alert(code);
   }
+
+  const downloadCodeFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([code], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "placeholder" + languageMap[readOnly? currTab:currTab-1].extension;
+    document.body.appendChild(element);
+    element.click();
+  }
+
+  const handleTabChange = (event, newTab) => {
+    setCurrTab(newTab);
+  };
+
+  const languageMap = [
+    { name: "Python", extension: ".py" },
+    { name: "Java", extension: ".java" },
+    { name: "C++", extension: ".cpp" },
+    { name: "Ruby", extension: ".rb" },
+    { name: "C#", extension: ".cs" },
+    { name: "Kotlin", extension: ".kt" },
+    { name: "Go", extension: ".go" },
+    { name: "Matlab", extension: ".m" },
+    // add more languages here
+  ];
+  
   return (
     <>
       <Container style={{"borderRadius": '5%', "padding": "6px", "backgroundColor": "white"}}>
-          <Editor 
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs 
+                value={currTab} 
+                onChange={handleTabChange} 
+                variant="scrollable"
+                scrollButtons="auto"
+              >
+                {!readOnly && <Tab label={"Detect Language"} key={0} />}
+                {languageMap.map((language, index) => (
+                  <Tab label={language.name} key={index + 1} />
+                ))}
+              </Tabs>
+            </Box>
+          </Box>
+          <Editor
             height="40vh" 
             theme="light" 
             defaultValue={defaultValue}
@@ -35,6 +81,16 @@ export default function CodeSubmissionBox({defaultValue, readOnly}) {
             <span>
               <Button variant="outlined" disabled={!inputExists} onClick={showValue}>Proof of concept to export code somewhere</Button>
             </span>
+          </Tooltip>
+          <Tooltip title={"Download"}>
+            <IconButton onClick={downloadCodeFile} aria-label="delete" size="large">
+              <DownloadRoundedIcon/>
+            </IconButton>
+          </Tooltip>
+          <Tooltip title={"Copy"}>
+            <IconButton onClick={() => {navigator.clipboard.writeText(code)}}>
+              <ContentCopyIcon/>
+            </IconButton>
           </Tooltip>
       </Container>
     </>
