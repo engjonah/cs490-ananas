980ad9ddaea47633fe9bc7c013517b362b18838b
Harshit Panwar <panwar.h.one@gmail.com>
2024-02-26 00:06:12 -0500

    [CA-73][CA-74] Added support for user registration through Google and Github (#22)
    [Problem]
    Needed to add support for users to use Google and Github accounts for registration
    [Solution]
    
    - Added Google and Github Authorization through Firebase Console
    - Created UI for sign-up options using MUI form and 2 buttons in `Sign-up.js` (needed to `npm install react-github-login-button` and `npm install react-google-button`
    - Moved sign-up form from the Translate page to a new page accessed by clicking the account button
    
    Co-authored-by: Jonah Eng <engjonah@gmail.com>

--- frontend/src/components/Sign-up.js
+++ frontend/src/components/Sign-up.js
@@ -1,85 +1,154 @@
 import React, {useState} from 'react';
-import { registerWithEmailAndPassword } from '../firebase';
- 
+import { useNavigate } from "react-router-dom";
+import { registerWithEmailAndPassword,signInWithGoogle,signInWithGithub } from '../firebase';
+import {Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container, Divider} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import GoogleButton from 'react-google-button';
+import GithubButton from 'react-github-login-button/dist/react-github-button'; 
 const SignUp = () => {
  
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
- 
-    const onSubmit = async (event) => {     
+    const navigate = useNavigate();
+
+    const onSubmitEmailPass = async (event) => {     
         event.preventDefault();
         try {
             await registerWithEmailAndPassword(name, email, password);
             console.log("done!")
+            navigate("/translate")
+        // Handle successful signup (e.g., redirect to protected content)
+        } catch (error) {
+            console.log(error.message);
+            alert(error.message);
+        }
+      
+    }
+    const onSubmitGoogle = async (event) => {
+      event.preventDefault();
+        try {
+            await signInWithGoogle();
+            console.log("done!")
+            navigate("/translate")
         // Handle successful signup (e.g., redirect to protected content)
         } catch (error) {
             console.log(error.message);
         }
-      
+    }
+    const onSubmitGithub = async (event) => {
+      event.preventDefault();
+        try {
+            await signInWithGithub();
+            console.log("done!")
+            navigate("/translate")
+        // Handle successful signup (e.g., redirect to protected content)
+        } catch (error) {
+            console.log(error.message);
+        }
     }
  
   return (
     <main >        
-        <section>
-            <div>
-                <div>                  
-                    <h1> SignUp </h1>                                                                            
-                    <form>
-                    <div>
-                        <label htmlFor="Name">
-                                Name
-                            </label>
-                            <input
-                                type="text"
-                                label="name"
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}  
-                                required                                    
-                                placeholder="Name"                                
-                            />
-                        </div>                                                                                            
-                        <div>
-                            <label htmlFor="email-address">
-                                Email address
-                            </label>
-                            <input
-                                type="email"
-                                label="Email address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}  
-                                required                                    
-                                placeholder="Email address"                                
-                            />
-                        </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={onSubmitEmailPass} sx={{ mt: 3, mb: 2}}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                    autoComplete="given-name"
+                    fullWidth
+                    name="Name"
+                    type="text"
+                    label="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}  
+                    required                                    
+                    placeholder="Name"
+                    autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                    required
+                    fullWidth
+                    type="email"
+                    label="Email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}  
+                    placeholder="Email address" 
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                    required
+                    fullWidth
+                    type="password"
+                    label="Create password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                    placeholder="Password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                type="submit"
+                variant="contained"
+                xs={12}
+                fullWidth
+                >
+                  Sign Up
+                </Button>
+              </Grid>           
+              <Grid container justifyContent="center" alignContent="center" margin={1}>
+                  <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link>    
+              </Grid>
+            </Grid>
+            <Divider color = "black" marginBottom = {2}/>
+          </Box>
+          <Box marginTop={2}>
+            <Grid container spacing={2}></Grid>
+            <GoogleButton
+                type="dark"
+                onClick={onSubmitGoogle}
+                variant="contained"
+              >
+                Sign Up With Google
+            </GoogleButton>
+            <GithubButton
+                type="dark"
+                onClick={onSubmitGithub}
+                variant="contained"
+              >
+                Sign Up With Github
+            </GithubButton>
+          </Box>
+          
+          
 
-                        <div>
-                            <label htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                label="Create password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)} 
-                                required                                 
-                                placeholder="Password"              
-                            />
-                        </div>                                             
-                        
-                        <button
-                            type="submit" 
-                            onClick={onSubmit}                        
-                        >  
-                            Sign up                                
-                        </button>
-                                                                     
-                    </form>
-                                    
-                </div>
-            </div>
-        </section>
+          
+        </Box>
+      </Container>
     </main>
+
+    
+
   )
 }
  
