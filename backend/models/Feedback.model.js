1af49ce6f8a2d000d4111ad5748eb38e3b618956
Amitesh Arya <61525462+AmiteshArya@users.noreply.github.com>
2024-02-26 20:14:30 -0500

    added mongodb schema for Feedback

---
+++ backend/models/Feedback.model.js
@@ -0,0 +1,40 @@
+const { Int32 } = require('mongodb');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const FeedbackSchema = new Schema({
+    uid:{
+        type: String,
+        required: true,
+    },
+    feedbackid:{
+        type: String,
+        required: true,
+    },
+    inputLang:{
+        type: String,
+        required: true,
+    },
+    outputLang:{
+        type: String,
+        required: true,
+    },
+    translationid:{
+        type: String,
+        required: true,
+    },
+    rating:{
+        type: Int32,
+        required: true,
+    },
+    review:{
+        type: String,
+        default: '',
+    }
+    
+});
+
+const Feedback = mongoose.model('Feedback', FeedbackSchema);
+
+module.exports = Feedback;
