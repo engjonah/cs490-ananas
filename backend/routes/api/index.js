c4dc7c967628084abda63e4e35ffec2af9fb6abb
Amitesh Arya <61525462+AmiteshArya@users.noreply.github.com>
2024-02-27 18:25:38 -0500

    [CA-50][CA-51] added controller to insert feedback (#28)
    [Problem]
    No way to insert feedback into database and needs api routing
    [Solution]
    - Created controller to receive and store feedback from web form.
    - added route to feedback controller.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 
 router.use('/test', require('./test'));
 router.use('/register', require('./register'))
+router.use('/feedback', require('./feedback'))
 module.exports = router;
