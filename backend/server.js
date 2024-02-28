6d8e674f667b0a0a79a71db052119dc4b1fb76c8
DantheOPMan <soccerdan34@gmail.com>
2024-02-28 00:17:01 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ require('dotenv').config();
 
 //link frontend on heroku
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
   });
 }
 
