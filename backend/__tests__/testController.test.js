0302d07ae7904ddac74fcb918e806dc1f49c7db5
Jonah Eng <engjonah@gmail.com>
2024-03-01 19:46:08 -0500

    add mock jwt token to backend test

--- backend/__tests__/testController.test.js
+++ backend/__tests__/testController.test.js
@@ -1,6 +1,11 @@
 const request = require("supertest");
 const app = require("../app");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+
+function generateMockToken() {
+  return jwt.sign({ userId: 'mockUserId' }, process.env.JWT_TOKEN_KEY, { expiresIn: '1h' });;
+}
 
 afterAll(done => {
   mongoose.connection.close()
@@ -9,8 +14,10 @@ afterAll(done => {
 
 describe('/api/test', () => {
   test("GET /api/test", async () => {
+    const token = generateMockToken();
     return await request(app)
       .get("/api/test")
+      .set('Authorization', `Bearer ${token}`)
       .expect(200)
       .expect('Content-Type', /json/)
       .then(response => {
