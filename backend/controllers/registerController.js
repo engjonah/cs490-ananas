2e70570964d0e6e5f032dbfa71ab0b1d50b0b3b9
Harshit Panwar <panwar.h.one@gmail.com>
2024-03-02 14:51:54 -0500

    Added backend unit test for register (#43)
    [Problem]
    Needed to test the `/api/register` endpoint in the backend
    [Solution]
    
    - Wrote backend test for registering endpoint
    - Needed to mock mongodb calls to avoid updating the database
    
    Co-authored-by: Jonah Eng <engjonah@gmail.com>

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -9,11 +9,11 @@ const registerNewUser = async(req,res) =>{
         console.log(`register new user received ${name,email,uid}`)
         const userFound = await User.findOne({uid});
         if (name == '' || email === ''){
-            throw Error("Please fill in all fields!")
+            return res.status(400).json({error:"Please fill in all fields!"})
         }else if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) == false ){
-            throw Error("Invalid email!")
+            return res.status(400).json({error:"Invalid email!"})
         }else if (uid == ''){
-            throw Error("Something went wrong with third-party sign in")
+            return res.status(400).json({error:"Something went wrong with third-party sign in"})
         }
         if (userFound){
             console.log('User already exists!')
@@ -26,11 +26,12 @@ const registerNewUser = async(req,res) =>{
             });
             await newUser.save();
             const token = jwt.sign({ uid }, process.env.JWT_TOKEN_KEY, {expiresIn: '2h'});
-            res.status(201).json({Message: "User registered!", uid, token})
+            return res.status(201).json({Message: "User registered!", uid, token})
         }
     }
     catch (error){
         console.log(`error occured : ${error.message}`)
+        return res.status(400).json({error: "Something went wrong!"})
     }
 };
 
