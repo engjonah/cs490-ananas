7b6a6e2bfaab1cb0c2b38271e8be509ec6bdc2a8
Amitesh Arya <61525462+AmiteshArya@users.noreply.github.com>
2024-03-20 18:10:26 -0400

    Update AuthContext.js

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -25,6 +25,7 @@ export const AuthContextProvider = ({children}) => {
         }
     },[])
     console.log('AuthContext state: ',state)
+    
     return (
         <AuthContext.Provider value = {{...state, dispatch}}>
             { children }
