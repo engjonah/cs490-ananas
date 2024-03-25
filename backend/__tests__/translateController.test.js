6bede2e51808277f9a10df76908b0fa42bb8e414
saumyad03 <97859804+saumyad03@users.noreply.github.com>
2024-03-24 21:47:51 -0400

    [CA-133][CA-128] Translate controller axios rework and unit tests (#76)
    [Problem]
    - Previously used OpenAI client to make requests to API
      - Made demonstrating rate limit error impossible
    - Made unit testing error status codes unfeasible (couldn't mock client
    nor intercept client's HTTP requests)
    
    [Solution]
    - Reworked controller to use axios instead
      - Actually runs into rate limit error
      - Allows us to mock HTTP responses with nock
    - Wrote unit tests with real API response and mocked API responses

---
+++ backend/__tests__/translateController.test.js
@@ -0,0 +1,73 @@
+const request = require("supertest");
+const app = require("../app");
+const Translation = require('../models/Translation.model');
+const mongoose = require("mongoose");
+const nock = require('nock')
+
+const baseURL = 'https://api.openai.com'
+const endpoint = '/v1/chat/completions'
+
+afterAll(done => {
+    mongoose.connection.close()
+    done()
+})
+
+describe('/api/translate real API requests', () => {
+    it("should return 200 status code and translation for successful translation and save translation", async () =>{
+        Translation.prototype.save = jest.fn()
+        return await request(app)
+        .post("/api/translate")
+        .send({uid: "Mock UID", inputLang: "Python", outputLang: "Java", inputCode: "print(\"Hello world\")", translatedAt: new Date()})
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(response => {
+            expect(response.body.translation).not.toBe("")
+            expect(Translation.prototype.save).toHaveBeenCalled()
+        })
+    })
+})
+
+describe('/api/translate mocked API requests', () => {
+    afterEach(() => {
+        nock.cleanAll()
+    })
+    it("should return 429 status code with error for rate limit exceeded", async () => {
+        nock(baseURL)
+        .post(endpoint)
+        .reply(429)
+        return await request(app)
+        .post("/api/translate")
+        .send({uid: "Mock UID", inputLang: "Python", outputLang: "Java", inputCode: "print(\"Hello world\")", translatedAt: new Date()})
+        .expect(429)
+        .expect('Content-Type', /json/)
+        .then(response => {
+            expect(response.body.error).toBe("Rate Limit Exceeded")
+        })
+    })
+    it("should return 503 status code with error for API connection error", async () => {
+        nock(baseURL)
+        .post(endpoint)
+        .reply(503)
+        return await request(app)
+        .post("/api/translate")
+        .send({uid: "Mock UID", inputLang: "Python", outputLang: "Java", inputCode: "print(\"Hello world\")", translatedAt: new Date()})
+        .expect(503)
+        .expect('Content-Type', /json/)
+        .then(response => {
+            expect(response.body.error).toBe("API Connection Error")
+        })
+    })
+    it("should return 500 status code with error for unknown error occurred", async () => {
+        nock(baseURL)
+        .post(endpoint)
+        .reply(500)
+        return await request(app)
+        .post("/api/translate")
+        .send({uid: "Mock UID", inputLang: "Python", outputLang: "Java", inputCode: "print(\"Hello world\")", translatedAt: new Date()})
+        .expect(500)
+        .expect('Content-Type', /json/)
+        .then(response => {
+            expect(response.body.error).toBe("Unknown Error Occurred")
+        })
+    })
+})
