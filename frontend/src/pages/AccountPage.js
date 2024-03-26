7ae7ec9a27dec3130fa55cbe91f1c1b971bade4a
Jaden Nguyen <21jadennguyen@gmail.com>
2024-03-25 22:51:06 -0400

    added translation history frontend

--- frontend/src/pages/AccountPage.js
+++ frontend/src/pages/AccountPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import AccountDetails from '../components/AccountDetails';
+import TranslationHistory from '../components/TranslationHistory';
+import { Grid } from '@mui/material';
 
 
 const AccountPage = () => {
   return (
-   <AccountDetails/>
+    <Grid container spacing={1} padding="50px" >
+      <AccountDetails/>
+      <TranslationHistory/>
+    </Grid>
   );
 }
 
