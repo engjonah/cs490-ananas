3fc03eee190e9f038efb9999d00816ecdd1ae7d7
Jonah Eng <engjonah@gmail.com>
2024-04-03 21:39:54 -0400

    [CA-191] Add user token to translate api call header (#104)
    [Problem]
    `/api/translate` call doesn't contain an auth token in the header,
    breaking current usage as it is now an auth protected endpoint.
    [Solution]
    Add token to api call header in CodeBox

--- frontend/src/components/CodeBox.js
+++ frontend/src/components/CodeBox.js
@@ -122,7 +122,8 @@ export default function CodeBox({ defaultValue, readOnly, outputLang, setOutputL
         translatedAt: new Date()
       }),
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
+        'Authorization':`Bearer ${user.token}`,
       },
     })
       .then((response) => {
