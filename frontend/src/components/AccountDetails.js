03112f19dd1cecfadb545512d387a60b0361115f
Amitesh Arya <61525462+AmiteshArya@users.noreply.github.com>
2024-04-05 00:28:02 -0400

    changed update name to be a proper form instead of a prompt

--- frontend/src/components/AccountDetails.js
+++ frontend/src/components/AccountDetails.js
@@ -16,7 +16,9 @@ const AccountDetails = () => {
   const [userInfo, setUserInfo] = useState(null);
   const [error, setError] = useState(null);
   const [passwordUpdateFormOpen, setPasswordUpdateFormOpen] = useState(false);
+  const [nameUpdateFormOpen, setNameUpdateFormOpen] = useState(false);
   const [newPassword, setNewPassword] = useState('');
+  const [newName, setNewName] = useState('');
   const [verifyNewPassword, setVerifyNewPassword] = useState('');
   const userId = JSON.parse(localStorage.getItem("user")).uid;
   const firstParty = firebaseOnlyUser();
@@ -61,8 +63,30 @@ const AccountDetails = () => {
     }
   };
 
+  const handleNameUpdateOpen = () => {
+    setNameUpdateFormOpen(true);
+  };
+
+  const handleNameUpdateClose = () => {
+    setNameUpdateFormOpen(false);
+  };
+
+  const handleNameUpdateSubmit = () => {
+    if (newName) {
+
+      setNewName(newName);
+      handleUpdateName();
+      handleNameUpdateClose(); 
+    } else {
+
+      toast.error('Name cannot be blank');
+      
+    }
+  };
+
   const handleUpdateName = () => {
-    const newName = prompt("Enter new name:");
+
+    
     const userId = JSON.parse(localStorage.getItem("user")).uid;
 
     if (newName) {
@@ -188,6 +212,29 @@ const AccountDetails = () => {
             </Button>
           </DialogActions>
         </Dialog>
+
+        <Dialog open={nameUpdateFormOpen} onCLose={handleNameUpdateClose}>
+        <DialogTitle>Change Name</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="New Name"
+            type="plaintext"
+            fullWidth
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleNameUpdateClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleNameUpdateSubmit} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+        </Dialog>
         <form style={{ width: '100%', marginTop: '16px' }} noValidate>
           
           <Button
@@ -196,7 +243,7 @@ const AccountDetails = () => {
             variant="contained"
             color="primary"
             style={{ marginTop: '16px' }}
-            onClick={handleUpdateName}
+            onClick={handleNameUpdateOpen}
           >
             Update Name
           </Button>
