1c73334c0e2e3ade78c9c6dabad3bbb8b72e5a2f
DantheOPMan <44145044+DantheOPMan@users.noreply.github.com>
2024-04-08 18:17:16 -0400

    [CA-165] Adjusted Feedback Form to have a character limit (#111)
    [Problem]
    Text input for feedback could be as long as possible
    
    [Solution]
    Added a hard cap of 1000 characters for submissions.

--- frontend/src/components/FeedbackForm.js
+++ frontend/src/components/FeedbackForm.js
@@ -10,13 +10,9 @@ import Rating from '@mui/material/Rating';
 import ApiUrl from '../ApiUrl';
 import { ErrorReport } from '../services/ErrorReport';
 import { useAuthContext } from '../hooks/useAuthContext';
-import { CssBaseline, Container} from '@mui/material';
+import { CssBaseline, Container, Box, Grid } from '@mui/material';
 import toast from 'react-hot-toast';
 
-
-
-
-  
 function FeedbackForm(props) {
   const [open, setOpen] = useState(false);
   const [review, setReview] = useState('');
@@ -25,7 +21,8 @@ function FeedbackForm(props) {
   const inputLang = props.inputLang;
   const uid = JSON.parse(localStorage.getItem("user"))?.uid;
   const translationId = props.translationId;
-  const {user} = useAuthContext()
+  const { user } = useAuthContext();
+  const characterLimit = 1000;
 
   const handleOpen = () => {
     setReview('');
@@ -45,30 +42,30 @@ function FeedbackForm(props) {
     setRating(newValue);
   };
 
-  const storeFeedback = async() =>{
-    await fetch(`${ApiUrl}/api/feedback`,{
-        method: "POST",
-        body: JSON.stringify({
-            uid,
-            inputLang,
-            outputLang,
-            translationId,
-            rating,
-            review,
-        }),
-        headers:{
-            "Content-type": "application/json",
-            'Authorization':`Bearer ${user.token}`,
-        },
+  const storeFeedback = async () => {
+    await fetch(`${ApiUrl}/api/feedback`, {
+      method: "POST",
+      body: JSON.stringify({
+        uid,
+        inputLang,
+        outputLang,
+        translationId,
+        rating,
+        review,
+      }),
+      headers: {
+        "Content-type": "application/json",
+        'Authorization': `Bearer ${user.token}`,
+      },
     })
-    .then(() => {
-        toast.success('Feedback Submitted!')
-    })
-    .catch((err) => {
+      .then(() => {
+        toast.success('Feedback Submitted!');
+      })
+      .catch((err) => {
         ErrorReport("Feedback Form:" + err.message);
-        console.log(err.message)
-    })
-};
+        console.log(err.message);
+      });
+  };
 
   const handleSubmit = () => {
     storeFeedback();
@@ -80,58 +77,62 @@ function FeedbackForm(props) {
       <Tooltip title={translationId ? "Submit Feedback" : "Translate something first!"}>
         <span>
           <Button disabled={!translationId} variant="contained" style={{ backgroundColor: '#CACACA', color: 'black'}} onClick={handleOpen}>
-          Feedback
-        </Button>
+            Feedback
+          </Button>
         </span>
       </Tooltip>
       <CssBaseline/>
-      <Dialog open={open} onClose={handleClose} maxWidth="xs" >
-        <DialogTitle
-          style={{
-            fontSize: '30px',
-            fontWeight: 'normal',
-            backgroundColor: '#d3d3d3', // Grey background color
-          }}
-        >
-          Rate This Translation
-            <Rating
-              name="feedback-rating"
-              value={rating}
-              onChange={(event, newValue) => {
-                handleRatingChange(newValue);
-              }}
-              size="large" // Make stars bigger
-            />
-        </DialogTitle>
-        <DialogContent
-          style={{
-            backgroundColor: '#d3d3d3',
-            paddingTop: '10px'
-          }}
-        >
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Leave a review"
-            type="text"
-            fullWidth
-            value={review}
-            onChange={handleReviewChange}
-            multiline={true}
-          />
-        </DialogContent>
-        <DialogActions
-        style={{ backgroundColor: '#d3d3d3'}}>
-          <Button onClick={handleClose} style={{color: 'black'}}>
-            Cancel
-          </Button>
-          <Button onClick={handleSubmit} style={{color: 'black'}}>
-            Submit
-          </Button>
-        </DialogActions>
+      <Dialog open={open} onClose={handleClose} maxWidth="xs">
+        <Box sx={{ width:'auto', maxWidth: '600px', backgroundColor: '#d3d3d3' }}>
+          <Grid container direction="column">
+            <Grid item >
+              <DialogTitle style={{ fontSize: '30px', fontWeight: 'normal', textAlign: 'center'}}>
+                Rate This Translation 
+              </DialogTitle>
+            </Grid>
+            <Grid item style={{textAlign:'center'}}>
+              <Rating
+                name="feedback-rating"
+                value={rating}
+                onChange={(event, newValue) => { handleRatingChange(newValue); }}
+                size="large"
+              />
+            </Grid>
+            <Grid item style={{width:'400px'}}>
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  label="Leave a review"
+                  type="text"
+                  fullWidth
+                  value={review}
+                  onChange={handleReviewChange}
+                  multiline={true}
+                  inputProps={{
+                    maxLength: characterLimit // Sets the maximum character length
+                  }}
+                  
+                  helperText={`${review.length}/${characterLimit} characters`} // Shows character count
+                />
+              </DialogContent>
+            </Grid>
+            <Grid item>
+              <DialogActions>
+                <Button onClick={handleClose} style={{color: 'black'}}>
+                  Cancel
+                </Button>
+                <Button onClick={handleSubmit} style={{color: 'black'}}>
+                  Submit
+                </Button>
+              </DialogActions>
+            </Grid>
+          </Grid>
+        </Box>
       </Dialog>
     </Container>
   );
 }
+
 
 export default FeedbackForm;
