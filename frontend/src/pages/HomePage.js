7071f1de74bbc708b9879752e8a8a892ca88e52f
Amitesh Arya <61525462+AmiteshArya@users.noreply.github.com>
2024-04-14 20:19:29 -0400

    added bar chart to the home page, working on resizing bug

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 import { Button, Grid } from '@mui/material';
 import './HomePage.css'; // Import the CSS file for HomePage
 
+
+
 const HomePage = () => {
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    scrollable: false,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+  const labels = ['1', '2', '3', '4', '5'];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'User Ratings',
+        data: [26,20,44,23,55],
+        backgroundColor: 'pink',
+      },
+
+    ],
+  };
+
   return (
     <div className="HomePage">
       <Grid container spacing={2} justifyContent="center" alignItems="center" className="HomePage-content">
@@ -25,6 +74,24 @@ const HomePage = () => {
           <Link to="/translate">
             <Button variant="contained" style={{ backgroundColor: '#CACACA', color: 'black', font:'Fira Code'}}>Translate Now!</Button>
           </Link>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} justifyContent="center" alignItems="center" className="HomePage-content">
+        <Grid item xs={12} sm={6}>
+          <h2>Reviews</h2>
+          <p>See what real users have to say about our service!</p>
+          {/* <Bar
+            options={options}
+            data={data}/> */}
+        </Grid>
+      </Grid>
+
+      <Grid container justifyContent="center" className="HomePage-feedback-bar-chart">
+        <Grid item>
+            <Bar
+            options={options}
+            data={data}/>
         </Grid>
       </Grid>
     </div>
