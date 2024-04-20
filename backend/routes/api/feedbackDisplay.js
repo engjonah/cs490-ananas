897abff788755ec5f0bf49d6672732c60cdd0561
Amitesh Arya <61525462+AmiteshArya@users.noreply.github.com>
2024-04-20 14:12:32 -0400

    Update feedbackDisplay.js
    Co-authored-by: Jonah Eng <engjonah@gmail.com>

--- backend/routes/api/feedbackDisplay.js
+++ backend/routes/api/feedbackDisplay.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const feedbackController = require('../../controllers/feedbackController');
-const requireAuth = require("../../middleware/requireAuth")
 
 router.get("/metrics", feedbackController.getFeedbackCountByRating)
 router.get("/", feedbackController.getFeedback)
