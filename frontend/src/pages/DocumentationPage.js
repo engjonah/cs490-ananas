7392568382edd25377a91080d878ba7d30c8a1b7
Amitesh Arya <61525462+AmiteshArya@users.noreply.github.com>
2024-04-20 23:06:41 -0400

    added 2fa blurb in doc page

--- frontend/src/pages/DocumentationPage.js
+++ frontend/src/pages/DocumentationPage.js
@@ -64,7 +64,7 @@ function DocumentationPage() {
         
         <h3>Authentication</h3>
         <p>
-          Authentication is handled through <b>Firebase API</b>. 
+          Authentication is handled through <b>Firebase API</b>. This includes user login and account creation as well as Two Factor Authentication.
         </p>
       </div>
       <div class="content">
